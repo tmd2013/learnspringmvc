b4d664349e287dde9d8e3cb9f212c8e1dc7e4b00
ffwang <ffwang@ffwangdeMac-mini.local>
2017-05-27 12:02:33 +0800

    helloworld创建

---
+++ learnspringmvc/src/main/java/com/ffwang/springmvc/heandles/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.ffwang.springmvc.heandles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+	@RequestMapping("/helloworld")
+	public String hello(){
+		System.out.println("成功");
+		return "succeed";
+	}
+}
